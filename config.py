eaf70c4d6da8ba15ccf35ab6073b589639d5fbee
rockedw <sonata7@qq.com>
2022-09-21 11:12:40 +0800

    新增module 大改

--- config.py
+++ config.py
@@ -12,10 +12,26 @@
 #     endpoint_url = 'http://169.254.123.2:9001/'  # minio server地址
 #     git_url = 'http://169.254.123.3:3000/'  # gitea地址
 
+# class Config:
+#     SQLALCHEMY_BINDS = {
+#         'mlflow': 'mysql://root:wangyan123@39.105.6.98:43307/mlflow',
+#         'gitea': 'mysql://root:wangyan123@39.105.6.98:43307/gitea'
+#     }
+#     SQLALCHEMY_TRACK_MODIFICATIONS = True
+#     SQLALCHEMY_COMMIT_TEARDOWN = True
+#     SQLALCHEMY_ECHO = True
+#     bucket_name = 'models'
+#     access_key = 'minioadmin'
+#     secret_key = 'minioadmin'
+#     endpoint_url = 'http://39.105.6.98:43099/'  # minio server地址
+#     git_url = 'http://39.105.6.98:43000/'  # gitea地址
+#     UPLOAD_FOLDER = r'./upload'
+#     HDFS_URL = 'http://172.31.129.149:50070'
+
 class Config:
     SQLALCHEMY_BINDS = {
-        'mlflow': 'mysql://root:wangyan123@39.105.6.98:43307/mlflow',
-        'gitea': 'mysql://root:wangyan123@39.105.6.98:43307/gitea'
+        'mlflow': 'mysql://root:wangyan123@8.130.105.10:59036/mlflow',
+        'gitea': 'mysql://root:wangyan123@8.130.105.10:59036/gitea'
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_TEARDOWN = True
